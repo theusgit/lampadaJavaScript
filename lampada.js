aafdba6f3543df1b264a947f0a774d9266b59c7f
theusgit <theusgit@outlook.com>
2021-01-18 20:50:55 -0300

    criando functions

---
+++ lampada.js
@@ -0,0 +1,15 @@
+
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+
+function lampOn(){
+    lamp.src='./img/ligada.jpg';
+}
+
+function lampOff(){
+    lamp.src='./img/desligada.jpg';
+}
+
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
