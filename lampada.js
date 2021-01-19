b962614cd8a4a2e94674cf4eb1b1c8f261f397f9
theusgit <theusgit@outlook.com>
2021-01-18 21:02:30 -0300

    Adicionando eventos

--- lampada.js
+++ lampada.js
@@ -11,5 +11,12 @@ function lampOff(){
     lamp.src='./img/desligada.jpg';
 }
 
+function lampBroken(){
+    lamp.src='./img/quebrada.jpg';
+}
+
 turnOn.addEventListener('click', lampOn);
 turnOff.addEventListener('click', lampOff);
+lamp.addEventListener('mouseover',lampOn);
+lamp.addEventListener('mouseleave',lampOff);
+lamp.addEventListener('dblclick',lampBroken);
