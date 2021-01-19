3f6f3fa97a489cb0a4b162d1873e15edc485f54e
theusgit <theusgit@outlook.com>
2021-01-18 21:13:42 -0300

    function isLampBroken

--- lampada.js
+++ lampada.js
@@ -3,12 +3,20 @@ const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp = document.getElementById('lamp');
 
+function isLampBroken(){
+    return lamp.src.indexOf('quebrada') >-1
+}
+
 function lampOn(){
+    if(!isLampBroken()){
     lamp.src='./img/ligada.jpg';
+    }
 }
 
 function lampOff(){
+    if(!isLampBroken()){
     lamp.src='./img/desligada.jpg';
+    }
 }
 
 function lampBroken(){
